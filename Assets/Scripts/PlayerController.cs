93d61eb50d0688a0f5676a27f0c986a7caf66d38
MoonfaceDev <elaicorem1@gmail.com>
2023-07-24 00:59:01 +0300

    Add Walk and PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Walk))]
+public class PlayerController : MonoBehaviour
+{
+    private Walk walk;
+
+    private void Awake()
+    {
+        walk = GetComponent<Walk>();
+    }
+
+    private void Update()
+    {
+        UpdateWalkDirection();
+    }
+
+    private void UpdateWalkDirection()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        walk.direction = new Vector2(horizontal, vertical).normalized;
+    }
+}
