3bd15a29434371543b9170b11c0b0b827b128fd9
MoonfaceDev <elaicorem1@gmail.com>
2023-07-26 02:06:46 +0300

    Fix dialog

---
+++ Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    public GameObject dialogPanel;
+    public TextMeshProUGUI text;
+    public float timeBetweenChars;
+
+    private const string ContinueStr = " >>";
+
+    private Queue<string> messageQueue;
+    private Coroutine messageCoroutine;
+
+    private void Awake()
+    {
+        messageQueue = new Queue<string>();
+    }
+
+    public void ShowMessages(IEnumerable<string> messages)
+    {
+        foreach (var message in messages)
+        {
+            messageQueue.Enqueue(message);
+        }
+        ShowNextMessage();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonUp("Continue"))
+        {
+            ShowNextMessage();
+        }
+    }
+
+    private void ShowNextMessage()
+    {
+        if (messageCoroutine != null)
+        {
+            StopCoroutine(messageCoroutine);
+        }
+
+        messageCoroutine = StartCoroutine(ShowMessageCoroutine());
+    }
+
+    private IEnumerator ShowMessageCoroutine()
+    {
+        if (messageQueue.Count == 0)
+        {
+            dialogPanel.SetActive(false);
+            yield break;
+        }
+        
+        dialogPanel.SetActive(true);
+
+        text.text = messageQueue.Dequeue();
+        text.ForceMeshUpdate();
+        
+        var totalVisibleCharacters = text.textInfo.characterCount;
+        var counter = 0;
+
+        while (true)
+        {
+            var visibleCount = counter % (totalVisibleCharacters + 1);
+            text.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                text.maxVisibleCharacters = visibleCount + 3;
+
+                var textTemp = text.text;
+                var textContinue = text.text + ContinueStr;
+
+                while (true)
+                {
+                    yield return new WaitForSeconds(0.3f);
+                    text.text = textContinue;
+                    yield return new WaitForSeconds(0.3f);
+                    text.text = textTemp;
+                }
+            }
+
+            counter += 1;
+            yield return new WaitForSeconds(timeBetweenChars);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/StartEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class StartEvent : MonoBehaviour
+{
+    public UnityEvent @event;
+
+    private void Start()
+    {
+        @event?.Invoke();
+    }
+}
