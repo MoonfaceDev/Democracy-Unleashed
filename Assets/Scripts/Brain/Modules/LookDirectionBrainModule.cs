4810201896cbc59efed2a1cad1b78e440d2383f8
MoonfaceDev <elaicorem1@gmail.com>
2023-07-26 00:24:59 +0300

    Add animation

---
+++ Assets/Scripts/Brain/Modules/LookDirectionBrainModule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brain Modules/Look Direction")]
+public class LookDirectionBrainModule : BrainModule
+{
+    private Walk walk;
+
+    public override void Create(Animator animator)
+    {
+        base.Create(animator);
+        walk = animator.GetComponent<Walk>();
+    }
+
+    public override void UpdateAnimator()
+    {
+        Animator.SetInteger("lookDirection", (int)walk.lookDirection);
+    }
+}
