8ec1103736bf058533dd46913bcccf73c45ae540
MoonfaceDev <elaicorem1@gmail.com>
2023-07-26 03:21:47 +0300

    Add protester

---
+++ Assets/Scripts/Behaviours/TakeDistance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Walk))]
+public class TakeDistance : MonoBehaviour
+{
+    public Collider2D closeRange;
+
+    private Walk walk;
+
+    private void Awake()
+    {
+        walk = GetComponent<Walk>();
+    }
+
+    private void FixedUpdate()
+    {
+        var overlappingColliders = new List<Collider2D>();
+        closeRange.OverlapCollider(new ContactFilter2D(), overlappingColliders);
+        var centerOfGravity = overlappingColliders
+            .Where(collider => collider.gameObject != gameObject)
+            .Select(collider => collider.transform.position)
+            .Aggregate(Vector3.zero, (average, position) => average + position) / overlappingColliders.Count;
+        walk.direction = (transform.position - centerOfGravity).normalized;
+    }
+}
---
+++ Assets/Scripts/Brain/Modules/TooCloseBrainModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brain Modules/Too Close")]
+public class TooCloseBrainModule : BrainModule
+{
+    public float range;
+
+    private Transform target;
+
+    public override void UpdateAnimator()
+    {
+        var colliders = FindObjectsOfType<Collider2D>()
+            .Where(collider => collider.gameObject != Animator.gameObject && !collider.isTrigger);
+        var closeColliders = colliders
+            .Where(collider => (collider.transform.position - Animator.transform.position).magnitude < range);
+        Animator.SetBool("tooClose", closeColliders.Any());
+    }
+}
