acec3b8f57a6cecb2b4943bea5113883234ac01e
MoonfaceDev <elaicorem1@gmail.com>
2023-07-29 02:01:17 +0300

    Fit DZ to game Add camera bounds

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,9 +4,16 @@ public class CameraFollow : MonoBehaviour
 {
     public float followDelay;
     public Transform target;
+    public Rect bounds;
 
     private Vector3 offset;
     private Vector3 velocity;
+
+    private static float CameraHeight => 2f * Camera.main.orthographicSize;
+    private static float CameraWidth => CameraHeight * Camera.main.aspect;
+    private static Vector2 CameraSize => new(CameraWidth, CameraHeight);
+
+    private Rect Bounds => new(bounds.min + CameraSize / 2, bounds.size - CameraSize);
 
     private void Awake()
     {
@@ -15,11 +22,22 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(
+        var smoothTarget = Vector3.SmoothDamp(
             transform.position,
             target.position + offset,
             ref velocity,
             followDelay
         );
+        transform.position = new Vector3(
+            Mathf.Clamp(smoothTarget.x, Bounds.xMin, Bounds.xMax),
+            Mathf.Clamp(smoothTarget.y, Bounds.yMin, Bounds.yMax),
+            smoothTarget.z
+        );
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(bounds.position + bounds.size / 2, bounds.size);
     }
 }
