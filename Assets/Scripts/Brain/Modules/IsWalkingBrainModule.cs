e3937acb52b3afb9836404090f1e2396cc355f89
MoonfaceDev <elaicorem1@gmail.com>
2023-07-28 17:26:36 +0300

    Add idle

---
+++ Assets/Scripts/Brain/Modules/IsWalkingBrainModule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brain Modules/Is Walking")]
+public class IsWalkingBrainModule : BrainModule
+{
+    private Walk walk;
+
+    public override void Create(Animator animator)
+    {
+        base.Create(animator);
+        walk = animator.GetComponent<Walk>();
+    }
+
+    public override void UpdateAnimator()
+    {
+        Animator.SetBool("isWalking", walk.direction != Vector2.zero);
+    }
+}
