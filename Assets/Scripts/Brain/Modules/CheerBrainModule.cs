d1aa775d618f35ecc14cfea0f3e6c56037c2029b
MoonfaceDev <elaicorem1@gmail.com>
2023-07-30 02:23:31 +0300

    Add cheer

---
+++ Assets/Scripts/Brain/Modules/CheerBrainModule.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CheerBrainModule : BrainModule
+{
+    private Morale playerMorale;
+
+    public override void Create(Animator animator)
+    {
+        base.Create(animator);
+        playerMorale = GameObject.FindGameObjectWithTag("Player").GetComponent<Morale>();
+        playerMorale.onLevelUp += () => animator.SetTrigger("cheer");
+    }
+
+    public override void UpdateAnimator()
+    {
+    }
+}
